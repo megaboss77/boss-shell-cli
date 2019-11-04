d161fc3b71057f1afedd8a218e50c3f22fd61bdc
Nattapat Sutapakorn <nattsut@th.ibm.com>
2019-11-04 16:37:33 +0700

    test1

---
+++ git.sh
@@ -0,0 +1,11 @@
+echo "# boss-shell-cli" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/megaboss77/boss-shell-cli.git
+git push -u origin master
+
+git add .
+git commit -m "hello"
+git push origin master 
+
